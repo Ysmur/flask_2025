178f8e8e62198dd93e6bfb146617e7b99496ba07
ysmur <ysmur@yandex.ru>
2025-02-28 18:06:11 +0300

    Урок 1 Задача 1 из др (изменен код в Изображение Марса)

--- Изображение Марса.py
+++ Изображение Марса.py
@@ -13,16 +13,49 @@ def return_sample_page():
                 <html lang="en">
                   <head>
                     <meta charset="utf-8">
+                    
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="../static/css/style.css"/>
+  </head>
                     <title>Привет, Марс!</title>
                   </head>
                   <body>
                     <h1>Жди нас, Марс!</h1>
                     <img src="{url_for('static', filename='img/mars.png')}" 
            alt="здесь должна была быть картинка, но не нашлась">
-                    <p> Вот она какая, красная планета.</p>
+
+                    <div class="alert alert-success" role="alert">
+                        <p>Вот она какая, красная планета.</p>
+                    </div>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+                    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+                    crossorigin="anonymous"></script>
                   </body>
                 </html>'''
 
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+    return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css" href="../static/css/style.css"/>
+                      </head>
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Мое предложение: {planet_name}!</h1>
+                      <h2> Эта планета</h2>
+
+                        <div class="alert alert-success" role="alert">
+                            <p>Вот она какая, красная планета.</p>
+                        </div>
+                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+                        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
+                        crossorigin="anonymous"></script>
+                      </body>
+                    </html>'''
 
 
 
