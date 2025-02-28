0145201546ca0715cebc5ac82844850f6917c154
ysmur <ysmur@yandex.ru>
2025-02-28 17:33:10 +0300

    рок 1 Задача 3 Изображение Марса

---
+++ Изображение Марса.py
@@ -0,0 +1,30 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def title():
+    return "Missiya"
+
+@app.route('/image_mars')
+def return_sample_page():
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/mars.png')}" 
+           alt="здесь должна была быть картинка, но не нашлась">
+                    <p> Вот она какая, красная планета.</p>
+                  </body>
+                </html>'''
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
