2c98abaa5fc442369e22daa662afca32cd014f81
ysmur <ysmur@yandex.ru>
2025-03-11 18:14:26 +0300

    Урок 2 Задача 3 (Список професий - base.html ist_prof.html и  server.py)

--- Урок 2_Шаблоны/server.py
+++ Урок 2_Шаблоны/server.py
@@ -12,6 +12,10 @@ def index(word):
 def training(prof):
     return render_template('training.html', prof=prof)
 
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    return render_template('list_prof.html', param=list)
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
