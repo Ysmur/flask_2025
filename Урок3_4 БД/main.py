cdc3f08003a017b93bbf036af824636cb999c9c0
ysmur <ysmur@yandex.ru>
2025-03-18 18:47:24 +0300

    Урок 3 Задача 2, 4

--- Урок3_4 БД/main.py
+++ Урок3_4 БД/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from data import db_session
 from data.users import User
+from data.jobs import Jobs
+import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -8,27 +10,20 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/mars_explorer.db")
-    user = User()
-    user.surname = "Ken"
-    user.name = "Rid"
-    user.age = 25
-    user.position = "cock"
-    user.speciality = "engineer"
-    user.address = "module_2"
-    user.email = "rid@mars.org"
-    user1 = User()
-    user1.surname = "Sid"
-    user1.name = "Tidley"
-    user1.age = 21
-    user1.position = "doctor"
-    user1.speciality = "doctor"
-    user1.address = "module_1"
-    user1.email = "sid@mars.org"
-    user1.hashed_password = 'sid'
+
+
+    job = Jobs()
+    job.team_leader = 1
+    job.job = "deployment of residential modules 1 and 2"
+    job.work_size = 15
+    job.collaborators = "2, 3"
+    job.start_date = datetime.datetime.now()
+    job.is_finished = False
     db_sess = db_session.create_session()
-    db_sess.add(user)
-    db_sess.add(user1)
+    db_sess.add(job)
     db_sess.commit()
+
+
     # app.run()
 
 
