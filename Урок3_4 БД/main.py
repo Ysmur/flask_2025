440d100b93c187cdcaf4f9fdeb095edc492a0eab
ysmur <ysmur@yandex.ru>
2025-03-18 19:01:41 +0300

    Урок 3 Задача Журнал работ

--- Урок3_4 БД/main.py
+++ Урок3_4 БД/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
@@ -7,24 +7,17 @@ import datetime
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
+@app.route('/')
+def index():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    print(jobs)
+    return render_template("index.html", jobs=jobs)
+
 
 def main():
     db_session.global_init("db/mars_explorer.db")
-
-
-    job = Jobs()
-    job.team_leader = 1
-    job.job = "deployment of residential modules 1 and 2"
-    job.work_size = 15
-    job.collaborators = "2, 3"
-    job.start_date = datetime.datetime.now()
-    job.is_finished = False
-    db_sess = db_session.create_session()
-    db_sess.add(job)
-    db_sess.commit()
-
-
-    # app.run()
+    app.run()
 
 
 
