371e205dc7fdc50d9626532a9eec52cd6e8b0682
ysmur <ysmur@yandex.ru>
2025-03-21 17:48:15 +0300

    Урок 3 Задача орма регистрации

---
+++ Урок3_4 БД/forms/user.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    surname = StringField('Фамилия пользователя', validators=[DataRequired()])
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    age = StringField('Возраст', validators=[DataRequired()])
+    position = StringField('Позиция')
+    speciality = StringField('Специальность')
+    address = StringField('Адрес', validators=[DataRequired()])
+    submit = SubmitField('Войти')
