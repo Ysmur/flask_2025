18664a44c139cc200df99883a1e7cea115da512c
ysmur <ysmur@yandex.ru>
2025-03-21 17:23:49 +0300

    Урок 2 Задача войная защита

---
+++ Урок 2_Шаблоны/loginform.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Id астронавта', validators=[DataRequired()])
+    password = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    username_cap = StringField('Id капитана', validators=[DataRequired()])
+    password_cap = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
--- Урок 2_Шаблоны/server.py
+++ Урок 2_Шаблоны/server.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from loginform import LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 @app.route('/<word>')
 @app.route('/index/<word>')
@@ -16,6 +18,13 @@ def training(prof):
 def list_prof(list):
     return render_template('list_prof.html', param=list)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=8880, host='127.0.0.1')
