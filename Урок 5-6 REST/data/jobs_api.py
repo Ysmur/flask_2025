64cbda0e619924ec3168838c5f5a49e6a6830bba
ysmur <ysmur@yandex.ru>
2025-03-25 19:00:05 +0300

    Урок 5 Задача Добавление работы (и успешный post запрос в test.py)

--- Урок 5-6 REST/data/jobs_api.py
+++ Урок 5-6 REST/data/jobs_api.py
@@ -1,5 +1,5 @@
 import flask
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 
 from . import db_session
 from .jobs import Jobs
@@ -35,3 +35,22 @@ def get_one_news(job_id):
             'job': job.to_dict(only=('job', 'team_leader', 'work_size'))
         }
     )
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create_jobs():
+    if not request.json:
+        return make_response(jsonify({'error': 'Empty request'}), 400)
+    elif not all(key in request.json for key in
+                 ['job', 'team_leader', 'work_size', 'collaborators', 'is_finished']):
+        return make_response(jsonify({'error': 'Bad request'}), 400)
+    db_sess = db_session.create_session()
+    job = Jobs()
+    job.job = request.json['job']
+    job.team_leader = request.json['team_leader']
+    job.work_size = request.json['work_size']
+    job.collaborators = request.json['collaborators']
+    job.is_finished = request.json['is_finished']
+    db_sess.add(job)
+    db_sess.commit()
+
+    return jsonify({'id': job.id})
--- Урок 5-6 REST/test.py
+++ Урок 5-6 REST/test.py
@@ -1,6 +1,16 @@
-from requests import get
+from requests import get, post
 
-print(get('http://localhost:5000/api/jobs').json()) # получение всех работ
-print(get('http://localhost:5000/api/jobs/2').json()) # получение одной существующей в бд работы
-print(get('http://localhost:5000/api/jobs/12').json()) # неверный id
-print(get('http://localhost:5000/api/jobs/stroka').json()) # id = строка
+# print(get('http://localhost:5000/api/jobs').json()) # получение всех работ
+# print(get('http://localhost:5000/api/jobs/2').json()) # получение одной существующей в бд работы
+# print(get('http://localhost:5000/api/jobs/12').json()) # неверный id
+# print(get('http://localhost:5000/api/jobs/stroka').json()) # id = строка
+
+print(post('http://localhost:5000/api/jobs',
+           json={'job': 'Заголовок',
+                 'work_size': 5,
+                 'team_leader': 1,
+                 'collaborators': '5, 7',
+                 'is_finished': False}).json())
+
+print(get('http://localhost:5000/api/jobs').json())
+
