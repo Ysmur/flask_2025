35867303f1386aad4a7445f2215c558235274b3f
ysmur <ysmur@yandex.ru>
2025-03-25 18:21:32 +0300

    Урок 5 Задача Получение всех работ

---
+++ Урок 5-6 REST/data/jobs_api.py
@@ -0,0 +1,25 @@
+import flask
+from flask import jsonify
+
+from . import db_session
+from .jobs import Jobs
+
+blueprint = flask.Blueprint(
+    'news_api',
+    __name__,
+    template_folder='templates'
+)
+
+
+@blueprint.route('/api/jobs')
+def get_news():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    print(jobs)
+    return jsonify(
+        {
+            'jobs':
+                [item.to_dict(only=('job', 'team_leader', 'work_size'))
+                 for item in jobs]
+        }
+    )
