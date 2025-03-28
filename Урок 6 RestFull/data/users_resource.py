140219d560ddbd0a21093b82a9cf6236bb6b2000
ysmur <ysmur@yandex.ru>
2025-03-28 17:42:27 +0300

    Урок 6 Задача Ресурс пользователи + запрос в test.py)

--- Урок 6 RestFull/data/users_resource.py
+++ Урок 6 RestFull/data/users_resource.py
@@ -2,6 +2,15 @@ from flask_restful import reqparse, abort, Api, Resource
 from . import db_session
 from .users import User
 from flask import jsonify
+from werkzeug.security import generate_password_hash
+
+parser = reqparse.RequestParser()
+parser.add_argument('surname', required=True)
+parser.add_argument('name', required=True)
+parser.add_argument('age', required=True, type=int)
+parser.add_argument('email', required=True)
+parser.add_argument('hashed_password', required=True)
+
 
 
 def abort_if_news_not_found(news_id):
@@ -10,6 +19,27 @@ def abort_if_news_not_found(news_id):
         news = session.query(User).get(news_id)
     if not news:
         abort(404, message=f"User {news_id} not found")
+
+class UsersListResource(Resource):
+    def get(self):
+        session = db_session.create_session()
+        news = session.query(User).all()
+        return jsonify({'users': [item.to_dict(
+            only=('surname', 'name', 'age')) for item in news]})
+
+    def post(self):
+        args = parser.parse_args()
+        session = db_session.create_session()
+        news = User(
+            surname=args['surname'],
+            name=args['name'],
+            age=args['age'],
+            email = args['email'],
+            hashed_password = generate_password_hash(args['hashed_password'])
+        )
+        session.add(news)
+        session.commit()
+        return jsonify({'id': news.id})
 
 
 class UsersResource(Resource):
--- Урок 6 RestFull/main.py
+++ Урок 6 RestFull/main.py
@@ -5,6 +5,7 @@ from flask_restful import reqparse, abort, Api, Resource
 app = Flask(__name__)
 api = Api(app)
 api.add_resource(users_resource.UsersResource, '/api/v2/users/<int:news_id>')
+api.add_resource(users_resource.UsersListResource, '/api/v2/users')
 
 def main():
     db_session.global_init("db/mars_explorer.db")
